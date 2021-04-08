4498753fdbdb45969f677605191149b1b2d158f7
Elias Wheatfall <ewheatfall@olin.edu>
2021-04-08 19:42:00 -0400

    Finished writing top-level comments

--- plots.py
+++ plots.py
@@ -22,11 +22,13 @@ def bar_graph(titles,x_data, y_data,color = "red"):
            color of plotted bars
 
     """
+    #Isolates variables from list to make them esier to use
     x_label = titles[0]
     y_label = titles[1]
     graph_title = titles[2]
     plt.rcParams["figure.figsize"] = (15, 10)
 
+    #Plot graph with lables and color
     plt.style.use("ggplot")
     plt.bar(x_data,y_data, color = color)
     plt.xlabel(x_label, fontsize = 24)
@@ -52,12 +54,14 @@ def pie_chart(slice_labels, sizes,end_index,start_index = 0):
     start_index: An int representing the start range of data to plot
                  (inclusive)
     """
-
+    #Select data range to plot based on given index
     label = slice_labels[start_index:end_index]
     sizes = [size/sum(sizes[start_index:end_index]) for size in \
         sizes[start_index:end_index]]
+    #Find index of largest size slice to explode
     explode_index =sizes.index(max(sizes))
 
+    #Explode largest slice to make more visually appealing
     for count, chunk in enumerate(sizes):
         if chunk < .02:
             label[count] = ""
@@ -65,9 +69,12 @@ def pie_chart(slice_labels, sizes,end_index,start_index = 0):
     explode = [0] * (len(label))
     explode[explode_index] = .05
 
+    #Set labels to empty string if they are too small to eliminate word
+    #overlapping
     def my_autopct(pct):
         return ("%1.1f%%" % pct) if pct > 4 else ""
 
+    #Plot pie chart with given paramters
     plt.rcParams["figure.figsize"] = (15, 15)
     plt.pie(sizes, explode=explode, labels=label, autopct=my_autopct, \
         shadow=False, startangle=90, normalize = False, textprops={"fontsize":24})
@@ -99,22 +106,27 @@ def violin_plot(headers,titles,data, splitlogic = True):
 
     Returns: None, displays a graph
     """
+    #Isolate data headers from list to make them easier to use
     data_header = headers[0]
     plot_index = headers[1]
     conditional_header = headers[2]
 
+    #Isolate labels from titles list to make them easier to use
     x_label = titles[0]
     y_label = titles[1]
     graph_title = titles[2]
+
+    #Set conditions of graph
     plt.rcParams["figure.figsize"] = (15, 10)
     plt.clf()
     sns.set_theme(style="whitegrid")
     sns.set(font_scale=2)
+
+    #Plot data with given parameters
     plot_one = sns.violinplot(x = plot_index,y = data_header, hue= conditional_header,data = data,\
          orient = "v", cut = 0, split=splitlogic, palette="rocket", )
     plot_one.set(xlabel=x_label)
     plot_one.set(ylabel=y_label)
-
     plot_one.set_title(graph_title)
     plt.show()
 
@@ -129,6 +141,7 @@ def color_changer(color_list):
     color_list: List of stings representing colors in hexadecimal, such as
                 (##0077ff)
     """
+    #Sets color cycle based on given colors
     plt.rcParams["axes.prop_cycle"] = plt.cycler(color=color_list)
 
 
@@ -138,6 +151,7 @@ def plot_format():
     Function used to format plots with a specific style. Called before
     plotting a graph.
     """
+    #Sets ideal plot paramters for graphs
     sns.set(font="Franklin Gothic Book",
         rc={
     "axes.axisbelow": False,
